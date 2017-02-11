0fe0ce08aa46e8b4481766b3d716e0199e7ab18b
Felipe Andrade <feandrad@gmail.com>
2017-02-11 02:28:28 -0300

    Api Calls

---
+++ app/src/main/java/feandrad/ombdexample/api/OMDbService.java
@@ -0,0 +1,18 @@
+package feandrad.ombdexample.api;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by feandrad on 11/02/2017.
+ */
+public class OMDbService {
+
+	public static OmdbApi.Search getSearchApi() {
+		return new Retrofit.Builder()
+				.baseUrl(OmdbApi.OMDB_BASE_URL)
+				.addConverterFactory(GsonConverterFactory.create())
+				.build()
+				.create(OmdbApi.Search.class);
+	}
+}
